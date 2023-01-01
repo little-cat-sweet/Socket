5350fb4bca0b076787328929d20207769ea7654c
Sky_Blue <2116400472@qq.com>
2023-01-01 20:13:58 +0800

    Flush

--- src/SecondHomework/UDP2.java
+++ src/SecondHomework/UDP2.java
@@ -1,7 +1,6 @@
 package SecondHomework;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
